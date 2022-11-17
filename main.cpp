e3d38694fcc35ae9566ec050cd9562864731d2f4
Nicola-besana <115076634+Nicola-besana@users.noreply.github.com>
2022-11-17 13:49:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   float a,b;
+    int opzione; 
+    cin>>a>>b>>opzione;
+    switch(opzione){
+        case 0: 
+            cout<<(a*b)/2;
+            break;
+        case 1: 
+            cout<<a*a;
+            break;
+        case 2: 
+            cout<<a*b;
+            break;
+        default:
+            cout<<"opzione non valida";
+            }
+            
+   
+   switch(opzione)
    return 0;
 }
